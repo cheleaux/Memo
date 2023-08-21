da76a3c049e15d330d5c89c05e682025ad628073
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-08-21 17:43:05 +0100

    fixed the bug using html onclick instead of event-listeners

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let date = getDate();
 let memoArray = localStorage.getItem('memos') ?
 JSON.parse(localStorage.getItem('memos')) : [];
 
-ul.addEventListener("click", deleteMemo);
 
 document.getElementById("clearBtn").addEventListener("click", () => {
     localStorage.clear();
@@ -26,7 +25,7 @@ memoArray.forEach(({ date, title, body }) => {
             <div class="card-body-container">
                 <p class="card-body">${body}</p>
             </div>
-            <div class="taskbar"><button class="deleteBtn"><i class="bi bi-trash3-fill"></i></button></div>
+            <div class="taskbar"><button class="deleteBtn" onclick="deleteMemo()"><i class="bi bi-trash3-fill"></i></button></div>
         </li>`;
     ul.insertAdjacentHTML("afterbegin", memoCard);
 });
@@ -43,7 +42,7 @@ function pinMemo(pin) {
             <div class="card-body-container">
                 <p class="card-body">${body.value}</p>
             </div>
-            <div class="taskbar"><button class="deleteBtn"><i class="bi bi-trash3-fill"></i></button></div>
+            <div class="taskbar"><button class="deleteBtn" onclick="deleteMemo()"><i class="bi bi-trash3-fill"></i></button></div>
         </li>`;
 
     let memoObj = {
@@ -59,24 +58,18 @@ function pinMemo(pin) {
     document.forms[0].reset();
 };
 
-
-
 function deleteMemo() {
-    if (event.target.classList.contains("deleteBtn")){
-        console.log("clicked")
-        // deletionTarget = event.target.closest(".memo-card");
-        // deletionTarget.remove();
-    }
-    // memoArray.splice(indexOfMemo, 1);
-    // localStorage.setItem('memos', JSON.stringify(memoArray));
-    // deletionTarget.remove();
+    const deletionTarget = event.target.closest(".memo-card");
+    indexOfMemo = Array.from(ul.children).indexOf(deletionTarget);
+    memoArray.splice(indexOfMemo, 1);
+    localStorage.setItem('memos', JSON.stringify(memoArray));
+    deletionTarget.remove();
 };
 
 function getDate(){
     let mm = new Date().toLocaleDateString("en-GB", { month: "2-digit" }).toString().slice(0, 2);
     let dd = new Date().toLocaleDateString("en-GB", { day: "2-digit" });
     let yy = new Date().getFullYear().toString().substr(-2);
-    let memoDate = `${dd} <div class="bullet"></div> ${mm} <div class="bullet"></div> ${yy}`
+    let memoDate = `${dd} <div class="bullet"></div> ${mm} <div class="bullet"></div> ${yy}`;
     return memoDate
-};
-
+}
